e7205e1b89b896bd3600e69b674de93ac7147507
Ahsan Tawil <ahsan.tawil07@gmail.com>
2020-11-26 16:41:19 +0700

    crud image

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -2,22 +2,30 @@ const { validationResult } = require('express-validator');
 const BlogPost = require('../models/db_blog');
 
 exports.createBlogPost = (req, res, next) => {
-    const title = req.body.title;
-    // const image = req.body.img;
-    const body = req.body.body;
-
+    
     const errors = validationResult(req);
-
+    
     if (!errors.isEmpty()) {
         const err = new Error('Invalid Value');
         err.errorStatus = 400;
         err.data = errors.array();
         throw err;
     } 
+    
+    if (!req.file) {
+        const err = new Error('Image Harus diupload');
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
 
     const post = new BlogPost({
         title: title,
         body: body,
+        image: image,
         author: {
             uid: 1,
             name: 'Ahsan Ta\'wil'
